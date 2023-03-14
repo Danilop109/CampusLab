09973d599121f070b8c1e926e70f7980f8ae8486
Daniela López <msdanilop109@gmail.com>
2023-03-14 18:29:42 -0500

    enseñanza

---
+++ js/script1.js
@@ -0,0 +1,8 @@
+console.log("Hola mundito de campus");
+console.log("      *  ***** ")
+console.log("      * *     *")
+console.log("      * *      ")
+console.log("      *  ***** ")
+console.log("      *       *")
+console.log("      * *     *")
+console.log("         ***** ")
